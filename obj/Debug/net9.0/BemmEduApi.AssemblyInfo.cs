16f848364e85465db4edd2c3b97e7316f792427f
Nguyễn Phúc Hậu <phuchau0608lt@gmail.com>
2024-12-15 23:18:12 +0700

    Update profile and login pages; change project target framework to .NET 9.0

--- obj/Debug/net9.0/BemmEduApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/BemmEduApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BemmEduApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb5b9a689bc35764ad5c89ee8e14ab9968d7b5b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17194c9d03fc441e8509303f6317dde77b4116f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BemmEduApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BemmEduApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
